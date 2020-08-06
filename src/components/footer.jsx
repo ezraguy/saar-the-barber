f3e4d415e9f8f43f47d8c59408db9664845c272d
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-08-06 20:38:52 +0300

    footer ui fix

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,14 +6,14 @@ const Footer = () => {
   return (
     <div className="container-fluid footer">
       <div className="row">
-        <div className="contnet col-5">
+        <div className="contnet col-lg-5 col-xl-5 col-12">
           <div id="mainTitle">Saar the barber</div>
-          <div className="footer-sub-title">
+          <div className="footer-sub-title gold">
             The best haircut you will ever get.
           </div>
         </div>
 
-        <div className="explore col">
+        <div className="explore col-lg-3 col-xl-3 col-12">
           <div className="title">Explore</div>
           <a href="#home">HOME</a>
           <a href="#aboutMe">about</a>
@@ -21,9 +21,8 @@ const Footer = () => {
           <a href="#gallery">gallery</a>
         </div>
 
-        <div className="lets-talk col">
+        <div className="lets-talk col-lg-4 col-xl-4 col-12">
           <div className="title">LET'S TALK</div>
-
           <PhoneSvg className="phone-svg" />
           <span>0548181959</span>
         </div>
